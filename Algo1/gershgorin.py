9459d97b7ccf6fad4be488b1582604b847eb4b88
Viktoriia21 <72379682+Viktoriia21@users.noreply.github.com>
2026-04-21 19:18:18 +0200

    Create gershgorin.py

---
+++ Algo1/gershgorin.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def gershgorin_disks(A):
+    centers = np.diag(A)
+    radii = np.sum(np.abs(A), axis=1) - np.abs(centers)
+    return centers, radii
+
+def gershgorin_box(A, padding=0.0):
+    centers, radii = gershgorin_disks(A)
+    xmin = np.min(centers.real - radii) - padding
+    xmax = np.max(centers.real + radii) + padding
+    ymin = np.min(centers.imag - radii) - padding
+    ymax = np.max(centers.imag + radii) + padding
+    return xmin, xmax, ymin, ymax
